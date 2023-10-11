4d14ae39ed17bd002a72e6f08ba32a2ab44283c1
Ragavi <129532617+Ragavipriya18@users.noreply.github.com>
2023-10-11 16:01:15 +0530

    Level II - Task 2

---
+++ Travel_Booking_System/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+      <div className='bg-black h-16 w-full rounded-b-3xl'>
+
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
